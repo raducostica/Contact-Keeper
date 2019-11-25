e73342d2d5ea8ec31b32d06362b6dbcf06c2d1b7
Radu Costica <radz1997c@gmail.com>
2019-11-25 21:29:48 +0000

    Prepare for deploy

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 // connectdb function
 const connectDB = require("./config/db");
 
+const path = require("path");
+
 const PORT = process.env.PORT || 5000;
 
 connectDB();
@@ -21,6 +23,16 @@ app.get("/", (req, res) => {
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`running on port ${PORT}`);
