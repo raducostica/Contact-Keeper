b8a36078b194993b394c1530a8a54cee94f7a0e5
Radu Costica <radz1997c@gmail.com>
2019-11-25 23:54:40 +0000

    fix

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,6 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
 
-    console.log(req.body);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
